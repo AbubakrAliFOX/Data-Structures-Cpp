edc5bbd6de0a212595246395043a28fc4e0d0eac
Abubakr <thefox5000@gmail.com>
2024-02-13 07:53:45 +0300

    Stack swap

--- Stack/file.cpp
+++ Stack/file.cpp
@@ -2,25 +2,38 @@
 #include <stack>
 using namespace std;
 
+int main()
+{
 
-int main () {
+    stack<int> Numbers1;
+    stack<int> Numbers2;
 
-    stack <int> Numbers;
+    Numbers1.push(10);
+    Numbers1.push(20);
+    Numbers1.push(30);
+    Numbers1.push(40);
+    Numbers1.push(50);
 
-    Numbers.push(10);
-    Numbers.push(20);
-    Numbers.push(30);
-    Numbers.push(40);
-    Numbers.push(50);
-    Numbers.push(60);
+    Numbers2.push(60);
+    Numbers2.push(70);
+    Numbers2.push(80);
+    Numbers2.push(90);
+    Numbers2.push(100);
 
-    cout << "Count: " << Numbers.size() << endl;
+    Numbers1.swap(Numbers2);
 
-    while (!Numbers.empty())
+    cout << "\nStack 1 after wap: " << endl;
+    while (!Numbers1.empty())
     {
-        cout << Numbers.top() << endl;
-        Numbers.pop();
+        cout << Numbers1.top() << endl;
+        Numbers1.pop();
     }
-     
+
+    cout << "\nStack 2 after swap: " << endl;
+    while (!Numbers2.empty())
+    {
+        cout << Numbers2.top() << endl;
+        Numbers2.pop();
+    }
     return 0;
 }
